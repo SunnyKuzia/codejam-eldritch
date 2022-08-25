e1ebd9a08d1d6c36a401778499a4379becb95bc4
Alexandra Huliakevich <sunnykuzia1996@gmail.com>
2022-08-25 22:59:53 +0300

    init: data preparation

---
+++ eldritch.js
@@ -0,0 +1,5 @@
+import greenCardsData from './data/mythicCards/green/index.js';
+import brownCardsData from './data/mythicCards/brown/index.js';
+import blueCardsData from './data/mythicCards/blue/index.js';
+import ancientsData from './data/ancients.js';
+console.log(greenCardsData);
